94edecb1c676afbb98d5229da5822c07a7b811d0
ErhanArda <erhanarda7@gmail.com>
2021-01-07 22:38:48 +0300

    json server and i18n added

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import VueTheMask from 'vue-the-mask'
+import i18n from './i18n'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3000/'
 
 Vue.use(VueTheMask)
 Vue.config.productionTip = false
@@ -26,5 +30,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
